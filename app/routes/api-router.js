c8907d9b818ebcb19fdbaf57c53b5286b37eebde
Michael Mesereau <michael.mesereau@vertafore.com>
2018-05-21 14:27:07 -0600

    swag 7

--- app/routes/api-router.js
+++ app/routes/api-router.js
@@ -67,75 +67,19 @@ module.exports = function(app, express) {
         .then(function(data) {
           res.status(201).json(JSON.parse(data[0].data));
         })
-      })
+        .catch(function(err) {
+          res.send(err);
+        });
+      });
 
     apiRouter.route('/contact')
         .post(function(req, res) {
           var mock_contact_string = req.body || JSON.stringify(mock_contact);
           knex('contact').insert({data: mock_contact_string}).returning('*')
           .then(function(data) {
-            console.log("successful insert");
+            console.log(data);
             res.status(201).json(
               JSON.parse(data[0])
-              // {
-              //   "content": {
-              //     "addresses": [
-              //       {
-              //         "address1": "999 18th St.",
-              //         "address2": "St. 600",
-              //         "city": "Denver",
-              //         "contactId": "string",
-              //         "countryCode": "US",
-              //         "createdDate": "2018-05-18T18:38:28.563Z",
-              //         "id": "string",
-              //         "postalCode": "80206",
-              //         "preferred": false,
-              //         "stateProvince": "CO",
-              //         "type": "string",
-              //         "updatedDate": "2018-05-18T18:38:28.563Z"
-              //       }
-              //     ],
-              //     "attributes": {
-              //       "additionalProp1": "string",
-              //       "additionalProp2": "string",
-              //       "additionalProp3": "string"
-              //     },
-              //     "createdDate": "2018-05-18T18:38:28.563Z",
-              //     "emails": [
-              //       {
-              //         "contactId": "string",
-              //         "createdDate": "2018-05-18T18:38:28.563Z",
-              //         "emailAddress": "jon.fishman@maine.gov",
-              //         "id": "string",
-              //         "preferred": false,
-              //         "updatedDate": "2018-05-18T18:38:28.563Z"
-              //       }
-              //     ],
-              //     "id": "string",
-              //     "name1": "Jim Davis",
-              //     "name2": "5th floor",
-              //     "phoneNumbers": [
-              //       {
-              //         "contactId": "string",
-              //         "countryCode": "1",
-              //         "createdDate": "2018-05-18T18:38:28.563Z",
-              //         "extension": "x5150",
-              //         "id": "string",
-              //         "phoneNumber": "303-555-1234",
-              //         "preferred": false,
-              //         "type": "string",
-              //         "updatedDate": "2018-05-18T18:38:28.563Z"
-              //       }
-              //     ],
-              //     "type": "Agency User",
-              //     "updatedDate": "2018-05-18T18:38:28.563Z"
-              //   },
-              //   "error": {
-              //     "message": "string",
-              //     "status": 0
-              //   },
-              //   "requestId": "string"
-              // }
             );
           })
           .catch(function(err) {
@@ -159,65 +103,6 @@ module.exports = function(app, express) {
           .catch(function(err) {
             res.send(err);
           });
-            // res.status(201).json({
-            //     "content": {
-            //       "addresses": [
-            //         {
-            //           "address1": "999 18th St.",
-            //           "address2": "St. 600",
-            //           "city": "Denver",
-            //           "contactId": "string",
-            //           "countryCode": "US",
-            //           "createdDate": "2018-05-18T17:20:49.992Z",
-            //           "id": "string",
-            //           "postalCode": "80206",
-            //           "preferred": false,
-            //           "stateProvince": "CO",
-            //           "type": "string",
-            //           "updatedDate": "2018-05-18T17:20:49.992Z"
-            //         }
-            //       ],
-            //       "attributes": {
-            //         "additionalProp1": "string",
-            //         "additionalProp2": "string",
-            //         "additionalProp3": "string"
-            //       },
-            //       "createdDate": "2018-05-18T17:20:49.992Z",
-            //       "emails": [
-            //         {
-            //           "contactId": "string",
-            //           "createdDate": "2018-05-18T17:20:49.992Z",
-            //           "emailAddress": "jon.fishman@maine.gov",
-            //           "id": "string",
-            //           "preferred": false,
-            //           "updatedDate": "2018-05-18T17:20:49.992Z"
-            //         }
-            //       ],
-            //       "id": "string",
-            //       "name1": "Jim Davis",
-            //       "name2": "5th floor",
-            //       "phoneNumbers": [
-            //         {
-            //           "contactId": "string",
-            //           "countryCode": "1",
-            //           "createdDate": "2018-05-18T17:20:49.992Z",
-            //           "extension": "x5150",
-            //           "id": "string",
-            //           "phoneNumber": "303-555-1234",
-            //           "preferred": false,
-            //           "type": "string",
-            //           "updatedDate": "2018-05-18T17:20:49.992Z"
-            //         }
-            //       ],
-            //       "type": "Agency User",
-            //       "updatedDate": "2018-05-18T17:20:49.992Z"
-            //     },
-            //     "error": {
-            //       "message": "string",
-            //       "status": 0
-            //     },
-            //     "requestId": "string"
-            //   });
         });
 
         apiRouter.route('/todo')
