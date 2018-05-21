2149cd0cf82eb4aa5bb2566f698158e4c8947b06
Michael Mesereau <michael.mesereau@vertafore.com>
2018-05-21 13:07:31 -0600

    pg 3

--- app/routes/api-router.js
+++ app/routes/api-router.js
@@ -1,7 +1,65 @@
 module.exports = function(app, express) {
     var apiRouter = express.Router();
     var knex = require('../../db/knex');
-    var mock_contact = require('/contact_data');
+    var mock_contact = {
+      "content": {
+        "addresses": [
+          {
+            "address1": "999 18th St.",
+            "address2": "St. 600",
+            "city": "Denver",
+            "contactId": "string",
+            "countryCode": "US",
+            "createdDate": "2018-05-18T18:38:28.563Z",
+            "id": "string",
+            "postalCode": "80206",
+            "preferred": false,
+            "stateProvince": "CO",
+            "type": "string",
+            "updatedDate": "2018-05-18T18:38:28.563Z"
+          }
+        ],
+        "attributes": {
+          "additionalProp1": "string",
+          "additionalProp2": "string",
+          "additionalProp3": "string"
+        },
+        "createdDate": "2018-05-18T18:38:28.563Z",
+        "emails": [
+          {
+            "contactId": "string",
+            "createdDate": "2018-05-18T18:38:28.563Z",
+            "emailAddress": "jon.fishman@maine.gov",
+            "id": "string",
+            "preferred": false,
+            "updatedDate": "2018-05-18T18:38:28.563Z"
+          }
+        ],
+        "id": "string",
+        "name1": "Jim Davis",
+        "name2": "5th floor",
+        "phoneNumbers": [
+          {
+            "contactId": "string",
+            "countryCode": "1",
+            "createdDate": "2018-05-18T18:38:28.563Z",
+            "extension": "x5150",
+            "id": "string",
+            "phoneNumber": "303-555-1234",
+            "preferred": false,
+            "type": "string",
+            "updatedDate": "2018-05-18T18:38:28.563Z"
+          }
+        ],
+        "type": "Agency User",
+        "updatedDate": "2018-05-18T18:38:28.563Z"
+      },
+      "error": {
+        "message": "string",
+        "status": 0
+      },
+      "requestId": "string"
+    };
 
     apiRouter.route('/contact')
         .post(function(req, res) {
