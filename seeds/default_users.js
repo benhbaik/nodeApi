18d8e5dc6da3b74fdab9670b8143d46dfea98026
Michael Mesereau <michael.mesereau@vertafore.com>
2018-05-23 10:29:52 -0600

    should add autosave to atom

--- seeds/default_users.js
+++ seeds/default_users.js
@@ -1,11 +1,5 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('contact').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('contact').insert([
-        {id: 1, data: 'Have some seed data!'}
-      ]);
-    });
+  return knex('contact').del();
 };
