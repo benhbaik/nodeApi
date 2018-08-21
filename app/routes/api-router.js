2436d71b0e41f92bc25d02816c97d3a1b31a7c9f
Ben Baik <ben.baik@vertafore.com>
2018-08-21 16:31:58 -0600

    changed request format

--- app/routes/api-router.js
+++ app/routes/api-router.js
@@ -3,27 +3,9 @@ module.exports = function(app, express) {
     var knex = require('../../db/knex');
     var mock_todo = {
         "content": {
-          "id": "string",
-          "action": "string",
-          "additionalDocumentIds": [
-            "string"
-          ],
           "assignedTo": "string",
-          "bestTimeToBeReached": "string",
-          "createdDate": "2018-05-18T18:52:02.453Z",
-          "description": "string",
-          "dueDate": "2018-05-18T18:52:02.453Z",
-          "emailAddress": "string",
-          "lobId": "string",
-          "lobType": "string",
-          "otherDetailsOrComments": "string",
-          "phoneNumber": "string",
-          "policyId": "string",
-          "policyNumber": "string",
-          "priority": "string",
-          "status": "string",
-          "type": "string",
-          "updatedDate": "2018-05-18T18:52:02.453Z"
+          "email": "string",
+          "phone": "string",
         },
         "error": {
           "message": "string",
@@ -34,56 +16,10 @@ module.exports = function(app, express) {
 
     var mock_contact = {
       "content": {
-        "addresses": [
-          {
-            "address1": "999 18th St.",
-            "address2": "St. 600",
-            "city": "Denver",
-            "contactId": "string",
-            "countryCode": "US",
-            "createdDate": "2018-05-18T18:38:28.563Z",
-            "id": "string",
-            "postalCode": "80206",
-            "preferred": false,
-            "stateProvince": "CO",
-            "type": "string",
-            "updatedDate": "2018-05-18T18:38:28.563Z"
-          }
-        ],
-        "attributes": {
-          "additionalProp1": "string",
-          "additionalProp2": "string",
-          "additionalProp3": "string"
-        },
-        "createdDate": "2018-05-18T18:38:28.563Z",
-        "emails": [
-          {
-            "contactId": "string",
-            "createdDate": "2018-05-18T18:38:28.563Z",
-            "emailAddress": "jon.fishman@maine.gov",
-            "id": "string",
-            "preferred": false,
-            "updatedDate": "2018-05-18T18:38:28.563Z"
-          }
-        ],
-        "id": "string",
-        "name1": "Jim Davis",
-        "name2": "5th floor",
-        "phoneNumbers": [
-          {
-            "contactId": "string",
-            "countryCode": "1",
-            "createdDate": "2018-05-18T18:38:28.563Z",
-            "extension": "x5150",
-            "id": "string",
-            "phoneNumber": "303-555-1234",
-            "preferred": false,
-            "type": "string",
-            "updatedDate": "2018-05-18T18:38:28.563Z"
-          }
-        ],
-        "type": "Agency User",
-        "updatedDate": "2018-05-18T18:38:28.563Z"
+        "email": "string",
+        "firstName": "string",
+        "lastName": "string",
+        "phone": "string",
       },
       "error": {
         "message": "string",
