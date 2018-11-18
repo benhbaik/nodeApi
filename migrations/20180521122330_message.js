24bf1e5b0de02f5611ff6077b61ddf60ed11312b
Ben Baik <ben.baik@vertafore.com>
2018-11-18 12:36:37 -0700

    added message migration

---
+++ migrations/20180521122330_message.js
@@ -0,0 +1,10 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('message', function(table) {
+    table.increments();
+    table.string('data', 20000);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('message');
+};
